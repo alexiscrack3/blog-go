a9619857a74e925692216c59143d90178cab9fa5
Alexis Ortega <alexis.oc3@gmail.com>
2021-04-04 23:33:42 -0600

    Fix GetPosts controller test

--- app/controllers/posts_controller_test.go
+++ app/controllers/posts_controller_test.go
@@ -1,35 +1,37 @@
 package controllers
 
 import (
-    // "encoding/json"
-    // "github.com/alexiscrack3/blog-go/app/repositories"
-    // "net/http"
-    // "net/http/httptest"
-    // "testing"
+    "encoding/json"
+    "github.com/alexiscrack3/blog-go/app/repositories"
+    "net/http"
+    "net/http/httptest"
+    "strings"
+    "testing"
 )
 
-// func Test_GetPosts_ShouldReturnPosts(t *testing.T) {
-//     postsRepository := repositories.NewPostsRepository()
-//     posts := postsRepository.GetPosts()
-//     testObject := NewPostsController(postsRepository)
+func Test_GetPosts_ShouldReturnPosts(t *testing.T) {
+    postsRepository := repositories.NewPostsRepository()
+    posts := postsRepository.GetPosts()
+    testObject := NewPostsController(postsRepository)
 
-// 	req, err := http.NewRequest("GET", "/posts", nil)
-// 	if err != nil {
-//         t.Fatal(err)
-// 	}
-// 	rr := httptest.NewRecorder()
-// 	handler := http.HandlerFunc(testObject.GetPosts)
-//     handler.ServeHTTP(rr, req)
+	req, err := http.NewRequest("GET", "/posts", nil)
+	if err != nil {
+        t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(testObject.GetPosts)
+    handler.ServeHTTP(rr, req)
 
-// 	if status := rr.Code; status != http.StatusOK {
-//         t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
-// 	}
+	if status := rr.Code; status != http.StatusOK {
+        t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
 
-// 	// Check the response body is what we expect.
-//     bytes, _ := json.Marshal(posts)
-//     expected := string(bytes)
-// 	if rr.Body.String() != expected {
-//         t.Errorf("handler returned unexpected body: got %v want %v",
-//         rr.Body.String(), expected)
-// 	}
-// }
+	// Check the response body is what we expect.
+    bytes, _ := json.Marshal(posts)
+    expected := string(bytes)
+    body := strings.TrimSpace(rr.Body.String())
+	if body != expected {
+        t.Errorf("handler returned unexpected body: got %v want %v",
+        rr.Body.String(), expected)
+	}
+}
